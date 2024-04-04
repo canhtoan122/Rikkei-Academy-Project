5d75bdf50e9d30338855961399cff39c5f6dc9d4
canhtoan122 <74394729+canhtoan122@users.noreply.github.com>
2024-04-04 15:50:43 +0700

    adding Bai Tap

--- Module 3/Session 03-express/ask-community-project/router/questions.js
+++ Module 3/Session 03-express/ask-community-project/router/questions.js
@@ -8,26 +8,26 @@ router.get('/', (req, res) => {
     res.send(data);
 });
 router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  let data = fs.readFileSync(`./dev-data/questions.json`, "utf8");
-  data = JSON.parse(data);
-  let result = data.findIndex((item, index, array) =>{
-    return item.id == id;
-  });
-  if(result == -1){
-    res.send("There are no user of that id.");
-  }else{
-    res.send(data[result]);
-  }
+    const { id } = req.params;
+    let data = fs.readFileSync(`./dev-data/questions.json`, "utf8");
+    data = JSON.parse(data);
+    let result = data.findIndex((item, index, array) => {
+        return item.id == id;
+    });
+    if (result == -1) {
+        res.send("There are no user of that id.");
+    } else {
+        res.send(data[result]);
+    }
 });
 router.post('/', (req, res) => {
     const { content, like, dislike } = req.body;
     let data = fs.readFileSync(`./dev-data/questions.json`, "utf8");
     data = JSON.parse(data);
-    let check = data.findIndex((item, index, array) =>{
+    let check = data.findIndex((item, index, array) => {
         return item.content == content;
     });
-    if(check == -1){
+    if (check == -1) {
         const newId = Date.now();
         let newObj = {
             content: content,
@@ -38,7 +38,7 @@ router.post('/', (req, res) => {
         data.push(newObj);
         fs.writeFileSync('./dev-data/questions.json', JSON.stringify(data));
         res.send("Create successfully");
-    }else{
+    } else {
         res.send("Question already exist!")
     }
 });
@@ -47,12 +47,12 @@ router.put('/:id', (req, res) => {
     let data = fs.readFileSync(`./dev-data/questions.json`, "utf8");
     data = JSON.parse(data);
     let newId = req.params.id;
-    let result = data.findIndex((item, index, array) =>{
+    let result = data.findIndex((item, index, array) => {
         return item.id == newId;
     });
-    if(result == -1){
+    if (result == -1) {
         res.send("Question not found!");
-    }else{
+    } else {
         let newObj = data[result];
         newObj.content = content;
         newObj.like = like;
@@ -64,14 +64,15 @@ router.delete('/:id', (req, res) => {
     let data = fs.readFileSync(`./dev-data/questions.json`, "utf8");
     data = JSON.parse(data);
     let newId = req.params.id;
-    let result = data.findIndex((item, index, array) =>{
-      return item.id == newId;
+    let result = data.findIndex((item, index, array) => {
+        return item.id == newId;
     });
-    if(result == -1){
-      res.send("Question not found.");
-    }else{
-        data.slice(result, 1)
-      res.send('Delete successfully!');
+    if (result == -1) {
+        res.send("Question not found.");
+    } else {
+        data.slice(result, 1);
+        fs.writeFileSync(`./dev-data/questions.json`, JSON.stringify(data));
+        res.send('Delete successfully!');
     }
-  });
+});
 module.exports = router
