5e063f990fb9f5fe7fbb1848e81f17db80c370c3
canhtoan122 <74394729+canhtoan122@users.noreply.github.com>
2024-05-17 18:06:17 +0700

    Adding Bai Tap

--- Module 4/session 01/index.ts
+++ Module 4/session 01/index.ts
@@ -62,3 +62,77 @@ function tinhtong(a:number, b:number):number{
     return a+b
 }
 tinhtong(1,5);
+// union
+let ourUnion:number | string =5;
+ourUnion = "5";
+// type alias (kiểu bí danh)
+type Code=string|number;
+function statusCode(code:Code):void{
+    console.log(`Status code is ${code}`);
+}
+statusCode(404);
+statusCode("404");
+
+type CarColor=String;
+type CarType = string;
+type Car = {
+    color:CarColor,
+    type:CarType,
+}
+const car:Car={
+    color:"green",
+    type:"honda",
+}
+// unknown-kiểu không xác định
+function test(params:unknown){
+    // Giả sử thầy đi sử lí cái tham số truyền vào
+    // Khi dùng kiểu dữ liệu unknown thì phải đi kiểm tra kiểu dữ liệu trước khi sử lí
+    if(typeof params == "string"){
+        let result = params.toUpperCase();
+        console.log(result);
+    }else{
+        console.log("Giá trị truyền vào không phải string");
+        
+    }
+}
+test("hello typescript!");
+// never: dùng trong các function xử lí các vòng lập vô tận, dùng để đếm
+function never():never{
+    let count = 0;
+    while(true){
+        count++;
+        console.log("Giá trị biến đếm count", count);
+    }
+}
+// type tuples - bộ dữ liệu(bộ fix cứng): tạo bộ khung cho dữ liệu mà không được phép sửa đổi
+let ourTuples:(number|string)[]=['1', 1];
+let ourTuples1:[string, number, boolean]=["hoa", 1, true];
+// type enum - liệt kê
+enum Role{
+    ADMIN,
+    USER
+}
+type Student2={
+    name:string,
+    age:number,
+    role:Role.ADMIN
+}
+let student2:Student2={
+    name:"minh thu",
+    age: 25,
+    role:0
+}
+// type intersection kiểu kết hợp &
+type A ={
+    name:string,
+    id: number
+}
+type B={
+    address:string,
+}
+type C = A&B;
+let person:C={
+    name:"hoa",
+    id:5,
+    address:"ha noi"
+}
